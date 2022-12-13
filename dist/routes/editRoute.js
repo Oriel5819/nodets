3981fe22086fb48ebfd88e47fe3dc9dbf6c4f6d9
alfred <dev.noc@telma.mg>
2022-12-13 17:17:55 +0300

    removing views

--- dist/routes/editRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.editRoute = void 0;
-const express_1 = require("express");
-// import { getList, changeStatus, setWatched } from "../controllers/localController";
-const editRoute = (0, express_1.Router)();
-exports.editRoute = editRoute;
-editRoute.get("/information", () => { });
-editRoute.post("/password", () => { });
